947c2eba538cbce617e9c0c928059414edb676de
Jan Gerhard Schøpp <jgschoepp@gmail.com>
2016-09-29 18:17:42 -0400

    Tried to fix sms bug

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
