68b0afe5c25642d2399b9d54feab42cd499389a9
Jan Gerhard Schøpp <jgschoepp@gmail.com>
2016-10-14 12:57:54 -0400

    Added question 1 and logic

--- server.js
+++ server.js
@@ -105,8 +105,20 @@ app.post('/receiveSMS', function(req, res) {
             counter++;
         }
 
+    } else if (counter == 2) { // Answering
+      var answer = parseInt(smsContent);
+      if (isNaN(answer) || (answer < 1 || answer > 3))
+          twiml.message('You have to input a number from 1 to 3!');
+      else {
+        if (answer != '1')
+          twiml.message('Unfortunatelly that is wrong.. Try again!');
+        else{
+            twiml.message('That is correct! Well done!');
+            counter++;
+        }
+      }
     } else {
-
+        twiml.message('For now that is all.. Text \'restart\' to start over!');
     }
 
     res.cookie('counter', counter);
@@ -122,13 +134,29 @@ function getQuizText(subject, counter) {
 
     switch (subject) {
         case 1: // Biology
-            text = 'You selected Biology!';
+            text = 'You selected Biology!' +
+            '\n\nQuestion 1: ' +
+            '\nWhich famous scientist introduced the idea of natural selection?' +
+            '\n1. Charles Darwin' +
+            '\n2. Albert Einstein' +
+            '\n3. Nicola Tesla';
             break;
         case 2: // Physics
-            text = 'You selected Physics!';
+            text = 'You selected Physics!'; +
+            '\n\nQuestion 1: ' +
+            '\nWhat is the force that holds back a sliding object?' +
+            '\n1. Friction' +
+            '\n2. Momentum' +
+            '\n3. Deceleration';
             break;
         case 3: // Maths
-            text = 'You selected Maths!';
+            text = 'You selected Maths!'; +
+            '\n\nQuestion 1: ' +
+            '\nA truck travels 225 km on the freeway at an average speed of 90 km/h.' +
+            '\nHow long does the journey take?' +
+            '\n1. 2 hours and 30 minutes' +
+            '\n2. 2 hours and 10 minutes' +
+            '\n3. 2 hours and 50 minutes';
             break;
         default:
             text = 'Something went wrong after selecting a subject.';
@@ -142,7 +170,8 @@ function getQuizText(subject, counter) {
 app.post('/receiveCall', function(req, res) {
 
     var stringResponse = 'Hi!  Thanks for giving me a call. ' +
-        'Yan has been pretty busy, and has not had time to actually set something up here. Thanks for calling anyways.!';
+        'Yan has been pretty busy, and has not had time to actually set something up here. ' +
+        'You could always try to text the codeword start to this number. Thanks for calling anyways.!';
 
     var twiml = new twilio.TwimlResponse();
 
@@ -158,5 +187,5 @@ app.post('/receiveCall', function(req, res) {
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('The app is running on http://localhost:' + port);
 });
