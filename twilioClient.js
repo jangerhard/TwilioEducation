20067539e6a8ce4644764e18c894650b519d89a5
Jan Gerhard Schøpp <jgschoepp@gmail.com>
2016-10-15 16:29:09 -0400

    Restructured slightly

--- twilioClient.js
+++ twilioClient.js
@@ -1,22 +1,23 @@
 var twilio = require('twilio');
 
 //Twilio stuff
-var accountSid = 'ACf4e148816559544ed7ce17003dcc37e5';
-var authToken = 'a54f023543553bdcac6fbada4f1b0a0c';
-var client = new twilio.RestClient(accountSid, authToken);
+var accountSid = 'ACf4e148816559544ed7ce17003dcc37e5',
+    authToken = 'a54f023543553bdcac6fbada4f1b0a0c',
+    client = new twilio.RestClient(accountSid, authToken),
+    twilioNum = "+12039894740";
 
 module.exports.sendSMS = function(to, message) {
-  client.messages.create({
-    body: message,
-    to: to,
-    from: "+12039894740",
-    // mediaUrl: 'http://www.yourserver.com/someimage.png'
-  }, function(err, data) {
-    if (err) {
-      console.error('Could not send sms!');
-      console.error(err);
-    } else {
-      console.log('Sms sent to: ' + to);
-    }
-  });
+    client.messages.create({
+        body: message,
+        to: to,
+        from: twilioNum,
+        // mediaUrl: 'http://www.yourserver.com/someimage.png'
+    }, function(err, data) {
+        if (err) {
+            console.error('Could not send sms!');
+            console.error(err);
+        } else {
+            console.log('Sms sent to: ' + to);
+        }
+    });
 };
